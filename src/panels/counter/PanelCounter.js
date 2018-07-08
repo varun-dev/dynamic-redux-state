546ba1a9f9f9646302a1e371ad3a60c8eb0c9287
Varun Dev <varun.dev@hotmail.com>
2018-07-08 10:06:02 +0100

    triggering incrementCount on panel initilisation
    The intial state is not getting set after moving to Redux 4, need to investigate further

--- src/panels/counter/PanelCounter.js
+++ src/panels/counter/PanelCounter.js
@@ -2,11 +2,27 @@ import React from 'react'
 import ButtonIncrement from './components/ButtonIncrement'
 import ButtonDecrement from './components/ButtonDecrement'
 import LabelCounter from './components/LabelCounter'
+import { incrementCount } from "./actions";
+import PropTypes from "prop-types";
+import {xconnect} from "../../utils/redux-utils";
 
-export default function PanelCounter() {
-  return <div>
-    <LabelCounter/>
-    <ButtonIncrement/>
-    <ButtonDecrement/>
-  </div>
+class PanelCounter extends React.Component {
+  componentDidMount () {
+    // TODO: investigate - Redux 4 not initialising reducer after replaceReducer API
+    this.props.incrementCount()
+  }
+
+  render () {
+    return <div>
+      <LabelCounter/>
+      <ButtonIncrement/>
+      <ButtonDecrement/>
+    </div>
+  }
+}
+
+export default xconnect (null, {incrementCount})(PanelCounter)
+
+PanelCounter.contextTypes = {
+  store: PropTypes.object.isRequired
 }
